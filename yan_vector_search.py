d9b486d1a80419fecee02f96cfc53ea1a5ab48d6
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-05-15 13:59:57 +0400

    Add files via upload

--- yan_vector_search.py
+++ yan_vector_search.py
@@ -1,54 +1,66 @@
 from milvus import *
 
+import yan_image_embedding
+
 milvus = Milvus(host='172.18.0.20', port='19530')
 
-try:
-  milvus.drop_collection(collection_name='uae_company_logo')
-except:
-  pass
 
-param = {'collection_name':'uae_company_logo', 'dimension':4096, 'index_file_size':1024, 'metric_type':MetricType.L2}
+########build the collection#########
+
+try:
+	milvus.drop_collection(collection_name='image')
+except:
+	pass
+
+param = {'collection_name':'image', 'dimension':4096, 'index_file_size':1024, 'metric_type':MetricType.L2}
 milvus.create_collection(param)
 milvus.list_collections()
 
+
+
 image_pathes = [
-'/Downloads/digital14_1.png',
-'/Downloads/digital14_2.jpg',
-'/Downloads/digital14_3.jpg',
-'/Downloads/mbzuai_1.png',
-'/Downloads/mbzuai_2.jpg',
-'/Downloads/mbzuai_3.png',
-'/Downloads/mbzuai_4.jpg',
-'/Downloads/darkmatter1.png',
-'/Downloads/darkmatter2.png',
-'/Downloads/g421.png',
-'/Downloads/g423.png',
-'/Downloads/g422.jpg',
-'/Downloads/iiai1.jpg',
-'/Downloads/iiai2.png',
-'/Downloads/iiai3.png',
+'digital14_1.png',
+'digital14_2.jpg',
+'digital14_3.jpg',
+'mbzuai_1.png',
+'mbzuai_2.jpg',
+'mbzuai_3.png',
+'mbzuai_4.jpg',
+'darkmatter1.png',
+'darkmatter2.png',
+'g421.png',
+'g423.png',
+'g422.jpg',
+'iiai1.jpg',
+'iiai2.png',
+'iiai3.png',
 ]
 
 image_mata = {}
 
 for i in image_pathes:
-	vector = image_to_vector(i)
-	result = milvus.insert(collection_name='uae_company_logo', records=[vector])
-	print(result)
+	vector = yan_image_embedding.image_to_vector(i)
+	result = milvus.insert(collection_name='image', records=[vector])
 	image_id = result[1][0]
 	image_path = i
 	image_mata[image_id] = image_path
 
-milvus.count_entities(collection_name='uae_company_logo')
+milvus.count_entities(collection_name='image')
+########build the collection#########
 
 
-##############
 
-queyr_image = '/Downloads/mbzuai_1.png'
-vector = image_to_vector(queyr_image)
+
+
+########query similar image######
+
+queyr_image = 'digital14_3.jpg'
+vector = yan_image_embedding.image_to_vector(queyr_image)
 
 search_param = {'nprobe': 16}
-search_result = milvus.search(collection_name='uae_company_logo', query_records=[vector], top_k = 100, params=search_param)
+search_result = milvus.search(collection_name='image', query_records=[vector], top_k = 100, params=search_param)
 
 for r in search_result[1][0]:
-	print(image_mata[r.id], r.distance)
+	print(image_mata[r.id], '\t',r.distance)
+
+########query similar image######
